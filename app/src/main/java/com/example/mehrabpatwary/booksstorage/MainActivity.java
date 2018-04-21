41807607b65a5d0a7be7f499a60dc7ddb104e97d
Mohsin Patwary <patwarydk@gmail.com>
2018-04-22 00:25:42 +0600

    activity_main compleate

--- app/src/main/java/com/example/mehrabpatwary/booksstorage/MainActivity.java
+++ app/src/main/java/com/example/mehrabpatwary/booksstorage/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Now i am testing my git practice
     }
 }
