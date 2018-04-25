f0d4c5eb2748f15c6af27eb25c6645a8e46ef28a
Mohsin Patwary <patwarydk@gmail.com>
2018-04-25 19:59:58 +0600

    Database update

--- app/src/main/java/com/example/mehrabpatwary/booksstorage/BookMarkHelper.java
+++ app/src/main/java/com/example/mehrabpatwary/booksstorage/BookMarkHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
-
+// database create by zahid
 public class BookMarkHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "Bookmark.db";
